bcae6ab38b180d970392b3034512e4d80dcb2e83
Thara Randhawa <thara.randhawa@motivss.com>
2018-09-21 13:38:46 -0700

    Latest working revision

--- debug_tool.py
+++ debug_tool.py
@@ -18,29 +18,34 @@
 import datetime
 import os
 import shutil
+import paramiko
 
+#operator IPs
 ap_ip = "169.168.0.200"
 lan_ip = "10.128.10.178"
 eth_ip = "10.42.2.2"
 vm_ip = "10.128.10.161"
 
 ip_hostname = dict([
-	(lan_ip, 'operator_lan')
-	(ap_ip, 'operator_ap')
-	(eth_ip, 'operator_eth')
+	(lan_ip, 'operator_lan'),
+	(ap_ip, 'operator_ap'),
+	(eth_ip, 'operator_eth'),
 	(vm_ip, 'operator_vm')
 ])
 
 date_ = str(datetime.date.today())
+operator_ = raw_input("Enter your name")
 time_ = (datetime.datetime.now().time().strftime("%H-%M-%S"))
 test_name = raw_input("Enter the name of this test:")
 
 roslog_dir = "/home/micra/.ros/log/latest/"
 home_dir = "/home/micra/"
-wks_dir = "/home/micra/micra_dev/src"
+wks_dir = "/home/micra/micra/src"
 path_ = home_dir + date_ + "_" + time_ + "_" + test_name
 
-#Create directory
+ssh = paramiko.SSHClient()
+
+#Create directory on High_Brain
 os.mkdir(path_)
 os.chdir(path_)
 
@@ -49,50 +54,6 @@ filename = str(date_ + " " + time_ + " " + test_name  + ".txt")
 file_ = open(filename,"w")
 
 descrip_ = raw_input("Write a short description of this run:")
-
-#Get ethercat config
-os.system("ethercat slaves | cat > ethercat_config")
-
-#Get master
-master_ = raw_input("What master will be running? l(ocal)/h(igh_brain)")
-cur_mstr = os.popen("echo $ROS_MASTER_URI").read()
-
-#Get operator
-cur_opr = os.popen("echo $ROS_IP")
-conn_who = os.popen("who").read()
-
-
-if lan_ip in conn_who:
-	new_opr = ip_hostname[lan_ip]
-elif vm_ip in conn_who:
-	new_opr = ip_hostname[vm_ip]
-elif ap_ip in conn_who:
-	new_opr = ip_hostname[ap_ip]
-elif eth_ip in conn_who:
-	new_opr = ip_hostname[eth_ip]	
-else:
-	print ("No Operator Connected!")
-	exit 1
-
-#Check current operator config
-ssh = paramiko.SSHClient()
-
-#Clear current operator
-if new_opr not curr_opr:
-	os.popen("cat ~/.bashrc | grep " + curr_opr).read()
-	if "#" 
-	os.system("sed -i s/export\ ROS_IP=" + cur_opr + "/#export\ ROS_IP=" + cur_opr)
-
-#Set new operator
-os.system("sed -i s/export\ ROS_IP=" + new_opr + "/")
-
-if master_ == "l":
-	operator_ = "localhost"
-else
-	operator_ = raw_input("What is the operator ID? ap/lan/vm/eth")
-
-#Set bash
-if operator_ == "ap":
 
 #Get run.py parameters
 runpy_param = raw_input("Enter the run.py configuration you wish to launch: ")
@@ -111,6 +72,53 @@ file_.write("\n")
 #Close File
 print (filename)
 file_.close()
+print("Test file written")
+
+#Get ethercat config from low_brain to high_brain
+print ("Retrieving ethercat status from low_brain")
+eth_cmd = "ethercat master | ssh high_brain 'cat > " + path_  + "/eth_config'"
+ssh.load_system_host_keys()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect("low_brain")
+st_in,st_out,st_err = ssh.exec_command(eth_cmd)
+ssh.close()
+
+#Get master
+master_ = raw_input("What master will be running? l(ocal)/h(igh_brain)")
+if master_ == 'l':
+	master_ = "localhost"
+elif master_ == 'h':
+	master_ = "high_brain"
+else:
+	print ("Master not recognized!")
+	exit()
+print (master_)
+
+#Get operator
+cur_opr = os.popen("echo $ROS_IP")
+conn_who = os.popen("who").read()
+
+
+if lan_ip in conn_who:
+	new_opr = ip_hostname[lan_ip]
+elif vm_ip in conn_who:
+	new_opr = ip_hostname[vm_ip]
+elif ap_ip in conn_who:
+	new_opr = ip_hostname[ap_ip]
+elif eth_ip in conn_who:
+	new_opr = ip_hostname[eth_ip]	
+else:
+	print ("No Operator Connected!")
+	exit()
+print(new_opr)
+
+#Append user bash with ROS_IP, ROS_HOSTNAME and ROS_MASTER_URI
+ssh.connect(new_opr)
+st_in,st_out,st_err = ssh.exec_command("sed -i 's/OPERATOR=" + new_opr + "/g' ~/.bashrc")
+st_in,st_out,st_err = ssh.exec_command("sed -i 's/MASTER=" + master_ + "/g' ~/.bashrc")
+st_in,st_out,st_err = ssh.exec_command("source ~/.bashrc")
+ssh.close()
+
 
 
 #Launch
@@ -135,7 +143,16 @@ zip_it = "zip -r " + folder_n + ".zip " + folder_n
 os.system(zip_it)
 
 #Try transferring test log to motiv servers
-smba_login = "smbclient //mss01-nas/users/ -W motivss.local -U trandhawa -A ~/.smbclient.conf -D trandhawa"
-smba_cmd = "-c \'lcd /home/micra; put " + folder_n + ".zip\'"
+if os.path.exists("~/.smbclient.conf"):
+	smba_login = "smbclient //mss01-nas/users/ -W motivss.local -U trandhawa -A ~/.smbclient.conf -D trandhawa"
+	smba_cmd = "-c \'lcd /home/micra; put " + folder_n + ".zip\'"
 
-os.system(smba_login + " " + smba_cmd)
+	os.system(smba_login + " " + smba_cmd)
+
+
+#Then try to the operator station
+else:
+	os.chdir(home_dir)
+	os.system("scp -r ./" + folder_n + ".zip " + new_opr + ":$HOME")
+
+
