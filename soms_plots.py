ae4cafdb767eac8987572de24c3c5e0d80428ed3
thara_randhawa <thara.randhawa@motivss.com>
2019-02-28 14:59:20 -0800

    Initial working version; csv stitching and plotting works

--- soms_plots.py
+++ soms_plots.py
@@ -34,6 +34,9 @@ import os
 HDR_START = 0
 UNITS = 0
 DATA_START = 1
+s_ = 0.5
+marker_ = "x"
+_dpi_ = 600
 
 X_1 = 'Time'
 Y_1 = 'Mtr1 mA'
@@ -49,12 +52,54 @@ class data_file:
 		self.units = 2
 		self.d_start = 3
 		self.testname = ''
+		self.date = ''
+		self.log_f = ''
 
 		self.time = pd.DataFrame()
 		self.curr = pd.DataFrame()
 		self.pwm = pd.DataFrame()
 
+# Calculate total seconds from weird DACEE timestamp
+def time_calc(time_stamp):
+   time_ = time_stamp.split(":")
+   hour_ = float(time_[0])
+   minute_ = float(time_[1])
+   second_ = float(time_[2])
+
+   time_ = hour_*3600 + minute_*60 + second_
+
+   return time_
+
 def plot():
+	file_data()
+	time_array = soms_.time.values
+	for i in range(len(soms_.time)):
+		time_array[i] = time_calc(time_array[i])
+	soms_.time = time_array
+
+	# title_ = current_datafile.data_.at[current_datafile.units, current_datafile.fields[y_column]] + \
+	        # " vs. " + current_datafile.data_.at[current_datafile.units, current_datafile.fields[x_column]]
+
+	os.chdir(soms_.directory)
+
+	plt.figure(1)
+	plt.title(soms_.log_f)
+	plt.ylabel(Y_1)
+	plt.xlabel(X_1)
+	plt.grid(True, which='major')
+
+	plt.scatter(soms_.time, soms_.curr, s=s_, marker = marker_)
+	plt.savefig(Y_1 + "_" + soms_.log_f + '.png', dpi = _dpi_)
+
+	plt.figure(2)
+	plt.title(soms_.log_f)
+	plt.ylabel(Y_2)
+	plt.xlabel(X_1)
+	plt.grid(True, which='major')
+
+	plt.scatter(soms_.time, soms_.pwm, s=s_, marker = marker_)
+	plt.savefig(Y_2 + "_" + soms_.log_f + '.png', dpi = _dpi_)
+
 	return
 
 def quit_all():
@@ -62,17 +107,19 @@ def quit_all():
 	master.destroy()
 
 def file_data():
-	soms_data.data_ = pd.DataFrame()
-	master.filename=askopenfilename()
-	soms_.filename = master.filename
+	log_dir = soms_.directory + "/" + soms_.log_f
 
-	soms_.testname = soms_data.filename.split('/')[-1]
+	soms_.data = pd.read_csv(log_dir, header=HDR_START)
 
-	soms_.data = pd.read_csv(soms_datafilename, header=HDR_START)
+	soms_.time = soms_.data.loc[1:, X_1]
+	# print ("Soms Time: \n")
+	# print (soms_.time)
+	soms_.curr = soms_.data.loc[1:, Y_1]
+	# print ("Soms Current: \n")
+	# print (soms_.curr)
+	soms_.pwm = soms_.data.loc[1:, Y_2]
 
-	soms_.time = soms_pd.loc[1:, X_1]
-	soms_.curr = soms_pd.loc[1:, Y_1]
-	soms_.pwm = soms_pd.loc[1:, Y_2]
+	return
 
 def sort_(array_):
 	for i in range(1,len(array_)):
@@ -83,6 +130,31 @@ def sort_(array_):
 			j = j-1
 		array_[j+1] = curr_
 	return array_
+
+def stitch_f(array_):
+	fname = 'Log_' + soms_.date + '.csv'
+	soms_.log_f = fname
+	os.chdir(soms_.directory)
+
+	if fname in os.listdir():
+		print ("Log file already exists!!")
+		quit_all()
+		return
+
+	else:
+		fout = open(fname,"a")
+
+		for line in open(array_[0]):
+			fout.write(line)
+		for file in range(1,len(array_)):
+			f = open(array_[file])
+			f.__next__()
+			for line in f:
+				fout.write(line)
+			f.close()
+		fout.close()
+		return
+
 
 def select_data():
 	master.directory=filedialog.askdirectory(title="Select directory with data...")
@@ -101,13 +173,18 @@ def select_data():
 			tests_.append(time_)
 	tests_ = sort_(tests_)
 
+	soms_.date=date_
 	csv_files = []
 
-	for tme_ in time_:
-		csv_files.append(master.directory + '/' + date_ + '_' + tme_)
+	for tme_ in tests_:
+		csv_files.append(master.directory + '/' + date_ + '_' + tme_ + '.csv')
 	
 
 	print (csv_files)
+	stitch_f(csv_files)
+
+	print ("Generating Plots...")
+	plot()
 
 master = Tk()
 
@@ -117,6 +194,7 @@ soms_ = data_file()
 
 # Launch GUI to select folder with days data
 Button(master, text='Select Data', command=select_data).pack()
+#Button(master, text='Generate Plots', command=plot).pack()
 Button(master, text='Quit', command=quit_all).pack()
 
 # Stitch together .csv files (just TIME, MTR1_MA, PWM1_%)
